0f1ab757c4e76bcd7ed1688a69242d72fdfefeb3
Bozhin Zafirov <bozhin@ankaa.opencode.com>
2013-03-20 17:04:26 +0200

    1. Strict files and directories mode change on update.

--- jail/update.py
+++ jail/update.py
@@ -14,24 +14,24 @@ def UpdateChroot(group='jail'):
 # update jail user directories
 def UpdateUser(user):
 	JailDirectories = (
-		('/home/{0}',				True),
-		('/jail/root/{0}',			False),
-		('/jail/home/{0}',			False),
-		('/jail/home/{0}/etc',			False),
-		('/jail/home/{0}/var',			False),
-		('/jail/home/{0}/var/log',		False),
-		('/jail/home/{0}/var/run',		False),
-		('/jail/home/{0}/tmp',			False),
-		('/jail/home/{0}/home',			False),
-		('/jail/home/{0}/home/{0}',		True),
-		('/jail/home/{0}/home/{0}/logs',	True),
-		('/jail/home/{0}/home/{0}/www',		True),
-		('/jail/home/{0}/home/{0}/mail',	True),
-		('/var/log/apache2/hosting/{0}',	False),
+		('/home/{0}',				True,		0750),
+		('/jail/root/{0}',			False,		0750),
+		('/jail/home/{0}',			False,		0750),
+		('/jail/home/{0}/etc',			False,		0755),
+		('/jail/home/{0}/var',			False,		0755),
+		('/jail/home/{0}/var/log',		False,		0755),
+		('/jail/home/{0}/var/run',		False,		0755),
+		('/jail/home/{0}/tmp',			False,		1777),
+		('/jail/home/{0}/home',			False,		0755),
+		('/jail/home/{0}/home/{0}',		True,		0750),
+		('/jail/home/{0}/home/{0}/logs',	True,		0755),
+		('/jail/home/{0}/home/{0}/www',		True,		0750),
+		('/jail/home/{0}/home/{0}/mail',	True,		0750),
+		('/var/log/apache2/hosting/{0}',	False,		0750),
 	)
 
 	# 3. create jail directories
-	for dirname, chown in JailDirectories:
+	for dirname, chown, mode in JailDirectories:
 		dirname = dirname.format(user)
 		if not os.path.exists(dirname):
 			os.makedirs(dirname)
@@ -40,12 +40,14 @@ def UpdateUser(user):
 					dirname,
 					pwd.getpwnam(user).pw_uid,
 					pwd.getpwnam(user).pw_gid)
+		os.chmod(dirname, mode)
 
 	# 4. create empty files
 	for f in (os.path.join('/jail/home', user, 'var/log/wtmp'),
 		os.path.join('/jail/home', user, 'var/log/utmp')):
 		if not os.path.exists(f):
 			open(f, 'w').close()
+		os.chmod(f, 0644)
 
 	# 5. generate custom passwd file
 	passwd_path = os.path.join('/jail/home', user, 'etc/passwd')
@@ -55,6 +57,7 @@ def UpdateUser(user):
 				[pw for pw in map(tuple, pwd.getpwall()) if
 					pw[2] <= 100 or pw[0] == user]]
 		open(passwd_path, 'w').write('\n'.join(map(':'.join, userdata))+'\n')
+		os.chmod(passwd_path, 0644)
 
 	# 6. generate custom group file
 	group_path = os.path.join('/jail/home', user, 'etc/group')
@@ -64,6 +67,7 @@ def UpdateUser(user):
 				map(tuple, grp.getgrall()) if
 					gr[2] <= 100 or gr[0] == user or user in gr[3]]
 		open(group_path, 'w').write('\n'.join(map(':'.join, groupdata))+'\n')
+		os.chmod(group_path, 0644)
 
 
 # update all jail user directories
