979a9ab93eff305d17d072ca818b8a3580d773a2
Bozhin Zafirov <bozhin.zafirov@factline.com>
2015-03-07 00:34:45 +0200

    Add groups setup option.

--- jail/setup.py
+++ jail/setup.py
@@ -1,5 +1,6 @@
 from platform import machine
 from subprocess import call
+from grp import getgrall
 
 # packages to install in chroot environment
 DefaultPackages = [
@@ -33,3 +34,29 @@ def ChrootUpgrade():
 		'/usr/sbin/chroot',
 		'/jail/base',
 		'apt-get', '-y', 'upgrade'])
+
+# setup jail groups
+def SetupGroups():
+	DefaultGroups = (
+		('jail',		990),
+		('jailtpe',		991),
+		('jailsocket',	992),
+		('jailcsocket',	993),
+		('jailssocket',	994))
+
+	SysGroups = getgrall()
+
+	for DefaultGroup, DefaultGroupID in DefaultGroups:
+		if DefaultGroup in map(lambda x: x.gr_name, SysGroups):
+			print 'Group with name %s already exists.' % DefaultGroup
+			continue
+		if DefaultGroupID in map(lambda x: x.gr_gid, SysGroups):
+			print 'Group with ID %d already exists.' % DefaultGroupID
+			continue
+		# add DefaultGroup with DefaultGroupID
+		groupadd = call([
+			'groupadd',
+			'-g', str(DefaultGroupID),
+			DefaultGroup])
+		if not groupadd:
+			print 'Created group %s with id %d' % (DefaultGroup, DefaultGroupID)
